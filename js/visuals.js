c4094b15cf6e9faddce123bf9a69b5dbecfdec13
Shirojyuu <shirojyuu@gmail.com>
2017-02-06 19:19:40 -0500

    Plotted timestamps on canvas

--- js/visuals.js
+++ js/visuals.js
@@ -2,25 +2,28 @@
 //The basic idea is using green and red circles to represent the transitivity of a relationship, plotting them over time.
 
 var canvas;
-var loader;
+var loadedCSV;
 
+var timestamps = [];
+var observations = [];
 
 $(document).ready
 {
     init();
 }
+
 function init()
 {
     
     canvas = document.getElementById('dataWindow');   
     var context = canvas.getContext("2d");
     
-    context.beginPath();
-    context.arc(20, 20, 5, 0, 360);
-    context.fillStyle = "blue";
-    context.fill();
-    console.log(canvas);
-    
+//    context.beginPath();
+//    context.arc(20, 20, 5, 0, 360);
+//    context.fillStyle = "blue";
+//    context.fill();
+//    console.log(canvas);
+//    
     //Open up the CSV file of choice
     $('#inputfile').change(function(data)
     {
@@ -31,9 +34,49 @@ function init()
             var data = e.target.result;
             var b64 = data.split('base64,')[1];
             var csv = atob(b64);
-            console.log(csv);
-
+            loadedCSV = csv;
+            parseCSV(loadedCSV);
+            plotOnCanvas();
         }
         reader.readAsDataURL(selectedFile);
+       
+        
     });
+}
+
+function parseCSV(inputFile)
+{
+    //Split the csv on a per-line basis.
+    //Data Order:
+    //  video #, time (sec), converted time, observation
+    
+    var lines = inputFile.split("\n");
+//    console.log(lines);
+    
+    for(var i = 0; i < lines.length; i++)
+    {
+        var components = lines[i].split(",");
+        if(components.indexOf("L") == -1)
+        {
+            timestamps.push(components[1]);
+            observations.push(components[3]);
+        }
+            
+    }
+    
+    console.log(timestamps);
+    console.log(observations);
+}
+
+function plotOnCanvas()
+{
+       var xOffset = 0;
+       var ctx = canvas.getContext("2d");
+       for(var i = 0; i < timestamps.length; i++)
+       {
+           ctx.beginPath();
+           ctx.arc(xOffset + timestamps[i], 50, 5, 0, 360);
+           ctx.fillStyle = "green"
+           ctx.fill()
+       }
 }
