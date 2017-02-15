a8aedebabf6f438509307427978dcb3b07d4677c
Shirojyuu <shirojyuu@gmail.com>
2017-02-14 20:59:16 -0500

    Functions for relating observations and finding transitive relats done; upgraded display

--- js/visuals.js
+++ js/visuals.js
@@ -6,6 +6,15 @@ var loadedCSV;
 
 var timestamps = [];
 var observations = [];
+var dataAvaialble = false;
+
+var fishRelations;
+
+//These are maps for each fish and their relation to another fish
+var connexA;
+var connexB;
+var connexC;
+var connexD;
 
 $(document).ready
 {
@@ -14,7 +23,13 @@ $(document).ready
 
 function init()
 {
-    
+    connexA = new Map(), connexB = new Map(), connexC = new Map(), connexD = new Map();
+    fishRelations = new Map();
+    fishRelations[1] = connexA;
+    fishRelations[2] = connexB;
+    fishRelations[3] = connexC;
+    fishRelations[4] = connexD;
+
     canvas = document.getElementById('dataWindow');   
     var context = canvas.getContext("2d");
     
@@ -36,12 +51,17 @@ function init()
             var csv = atob(b64);
             loadedCSV = csv;
             parseCSV(loadedCSV);
-            plotOnCanvas();
+            plotTimepoints();
         }
         reader.readAsDataURL(selectedFile);
        
         
     });
+
+    if(dataAvaialble == true)
+    {
+        console.log("All done!");
+    }
 }
 
 function parseCSV(inputFile)
@@ -64,24 +84,165 @@ function parseCSV(inputFile)
             {
                 timestamps.push(tStamp);
                 observations.push(obs);
+                plotOnCanvas();
+                mapRelation(obs);
             }
         }
             
     }
     
-    console.log(timestamps);
-    console.log(observations);
+    // console.log(timestamps);
+    // console.log(observations);
+    dataAvaialble = true;
+}
+
+//Plots little ticks to represent units of time.
+function plotTimepoints()
+{
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.rect(0, 110, canvas.width, 1);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+
+
+
+    ctx.font = "10px Arial";
+    for(var i = 0; i < canvas.width; i++)
+    {
+        if(i % 50 == 0)
+        {
+            ctx.rect(i, 115, 0.4, 5);
+            ctx.fillStyle = "#000000";
+            ctx.fill();
+            ctx.fillText(i, i + 1, 130);
+        }
+
+    }
+        ctx.closePath();
+
 }
 
 function plotOnCanvas()
 {
        var xOffset = 0;
        var ctx = canvas.getContext("2d");
-       for(var i = 0; i < timestamps.length; i++)
-       {
+
            ctx.beginPath();
-           ctx.arc(xOffset + timestamps[i], 50, 5, 0, 360);
-           ctx.fillStyle = "green"
+           ctx.arc(xOffset + timestamps[timestamps.length - 1], 50, 5, 0, 360);
+
+           var color = "#000000";
+        //    Do some testing here
+           if(isTransitive(connexC, connexB, connexA, "2", "1"))
+           {
+               color = "#00ff00";
+           }
+
+           else
+           {
+               var color = "#000000";
+           }
+            
+           ctx.fillStyle = color;
            ctx.fill()
-       }
+           ctx.closePath();
+}
+
+//Takes an observation, ob, and builds a mapping between the acting fish and receiving fish
+function mapRelation(ob)
+{
+    //Index 0 = Actor, Index 1 = Action, Index 2 = Receiver
+    var interactions = ob.split(" ");
+    var act = interactions[0];
+    var rcv = interactions[2];
+
+    switch(act)
+    {
+        case "1":
+            if(rcv == "2")
+            {
+                connexA[rcv] = 1;
+                connexB[act] = 0;
+            }
+            else if(rcv == "3")
+            {
+                connexA[rcv] = 1;
+                connexC[act] = 0;
+            }
+            else if(rcv == "4")
+            {
+                connexA[rcv] = 1;
+                connexD[act] = 0;
+            }
+            break;
+        
+        case "2":
+            if(rcv == "1")
+            {
+                connexB[rcv] = 1;
+                connexA[act] = 0;
+            }
+            else if(rcv == "3")
+            {
+                connexB[rcv] = 1;
+                connexC[act] = 0;
+            }
+            else if(rcv == "4")
+            {
+                connexB[rcv] = 1;
+                connexD[act] = 0;
+            }
+            break;
+
+        case "3":
+            if(rcv == "1")
+            {
+                connexC[rcv] = 1;
+                connexA[act] = 0;
+            }
+            else if(rcv == "2")
+            {
+                connexC[rcv] = 1;
+                connexB[act] = 0;
+            }
+            else if(rcv == "4")
+            {
+                connexC[rcv] = 1;
+                connexD[act] = 0;
+            }
+            break;
+
+        case "4":
+            if(rcv == "1")
+            {
+                connexD[rcv] = 1;
+                connexA[act] = 0;
+            }
+            else if(rcv == "2")
+            {
+                connexD[rcv] = 1;
+                connexB[act] = 0;
+            }
+            else if(rcv == "3")
+            {
+                connexD[rcv] = 1;
+                connexC[act] = 0;
+            }
+            break;
+    }
+}
+
+//Takes 3 connex maps and then checks entries against each other for transitivity.
+function isTransitive(cm1, cm2, cm3, cv2, cv3)
+{
+    if(cm1[cv2] == 1)
+    {
+        if(cm2[cv3] == 1)
+        {
+            if(cm1[cv3] == 1)
+                return true;
+        }
+    }
+
+    return false;
 }
